4f4d5df02a3267f68f068bbd3f68f4ee9819f113
Hiren Patel <hiren6@gmail.com>
2021-10-27 16:52:40 +1300

    second commit

---
+++ interaction.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+URL = "http://secure-retreat-92358.herokuapp.com/"
+
+
+chrome_driver_path = "/home/hiren/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get(URL)
+
+fname = driver.find_element_by_name("fName")
+
+fname.send_keys("Hiren")
+lname = driver.find_element_by_name("lName")
+lname.send_keys("Patel")
+email = driver.find_element_by_name("email")
+email.send_keys("Hiren6@gmail.com")
+
+sign_up = driver.find_element_by_css_selector(".btn-primary")
+sign_up.send_keys(Keys.ENTER)
+
